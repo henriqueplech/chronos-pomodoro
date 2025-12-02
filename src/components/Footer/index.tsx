cbf2fe4ac8a30543d46b69a713ff88159610da4d
henriqueplech <henriqueplech1@gmail.com>
2025-12-02 11:25:55 -0300

    teste

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,7 +8,7 @@ export function Footer() {
         Entenda como funciona a técnica pomodoro
       </RouterLink>
       <RouterLink href='/'>
-        Chronos Pomodoro &copy; {new Date().getFullYear()} - Feito com 💚
+        Chronos Pomodoro &copy; {new Date().getFullYear()} - AGORA VAI NANDINHO!
       </RouterLink>
     </footer>
   );
